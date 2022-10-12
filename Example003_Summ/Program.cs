89e7a8b88acaf14767fe488333d973f87d74da5f
Pijama_sem <example@example.com>
2022-10-12 23:10:23 +0300

    Add Random

--- Example003_Summ/Program.cs
+++ Example003_Summ/Program.cs
@@ -1,4 +1,6 @@
-﻿int nubmberA=31;
-int nubmberb=3;
-int result = nubmberA+nubmberb;
+﻿int numberA=new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberb=new Random().Next(1,10);
+Console.WriteLine(numberb);
+int result = numberA+numberb;
 Console.WriteLine(result);
